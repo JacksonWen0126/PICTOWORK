de8886847c39ac0e5e975f72b3775978cde4509c
Jackson Wen <Jacksonwen026@gmail.com>
2019-07-16 11:09:44 -0500

    Add more examples

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PICTOWORD
 Using Baidu API picture reader convert to word
+Support PNG, JPG, JPEG, BMP
--- pictword.py
+++ pictword.py
@@ -5,7 +5,15 @@ import base64,urllib.parse,urllib.request,urllib.error,json
 access_token = '24.14d4cf2e93345ddfe663236f90aaddb1.2592000.1565819100.282335-16814012'
 url = 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token=' + access_token
 
-f = open(r'pic1.png', 'rb')
+fname = input("Enter '1' for png '2' for jpg '3' for bmp:\n")
+if fname == "2":
+    filename = "Chinese-Test-Translation.jpg"
+elif fname == "3":
+    filename = "Elements-English.bmp"
+else:
+    filename = 'pic1.png'
+print("Please wait")
+f = open(filename, 'rb')
 # encode picture to base64
 img = base64.b64encode(f.read())
 params = {"image": img}
